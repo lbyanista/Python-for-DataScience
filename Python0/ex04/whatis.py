77a5350ba5bc7bedfb5d664f40284e4029c94d7b
Mohamed fadel Labrayji <mlabrayj@e2r2p1.1337.ma>
2023-10-11 08:16:31 +0100

    add 04 still -int

---
+++ Python0/ex04/whatis.py
@@ -0,0 +1,8 @@
+import sys
+
+assert len(sys.argv) == 2, "more than one argument is provided"
+assert sys.argv[1].isdigit(), "argument is not an integer"
+if int(sys.argv[1]) % 2 == 0:
+    print("I'm Even.")
+else:
+    print("I'm Odd.")
