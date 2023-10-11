b9a2d7667a733197c32a930f098502dc3113ce6f
Mohamed fadel Labrayji <mlabrayj@e1r1p3.1337.ma>
2023-10-11 18:50:45 +0100

    edit ex04 p00

--- Python0/ex04/whatis.py
+++ Python0/ex04/whatis.py
@@ -1,8 +1,16 @@
 import sys
 
-assert len(sys.argv) == 2, "more than one argument is provided"
-assert sys.argv[1].isdigit(), "argument is not an integer"
-if int(sys.argv[1]) % 2 == 0:
-    print("I'm Even.")
-else:
-    print("I'm Odd.")
+try:
+    if len(sys.argv) == 1:
+        exit()
+    assert len(sys.argv) == 2, "AssertionError: more than one argument is provided"
+    arg = sys.argv[1]
+    if arg[0] == '-' and arg[1:].isdigit() or arg.isdigit():
+        if int(arg) % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    else:
+        assert False, "AssertionError: argument is not an integer"
+except AssertionError as error:
+    print(error)
